210fc46aa0b6b226f44fb05d9bcdd0c18296e88b
Marcela-Sevilla <marcelasevilla514@gmail.com>
2023-02-20 16:54:55 -0500

    Creando Calculadora

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -10,13 +10,16 @@ export class PersonasComponent{
     deshabilitar = false;
     mensaje = 'Da click para ver el mensaje!';
     titulo = 'Ingeniero';
+    total = 0;
+    valorA = 0;
+    valorB = 0;
 
     mostrarMensaje(){
         this.mensaje = 'Bienvenido a mi primera App en Angular.';
     }
 
     calcularSuma(){
-        
+        return this.total = this.valorA + this.valorB;
     }
 
 
