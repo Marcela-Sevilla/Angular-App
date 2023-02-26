9c076d099023661f9586e9c48f1860189348518f
Marcela-Sevilla <marcelasevilla514@gmail.com>
2023-02-25 21:26:41 -0500

    Modularizando la aplicación en la sección de calculadora

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppComponent } from './app.component';
 import { PersonasComponent } from './personas/personas.component';
 import { PersonaComponent } from './persona/persona.component';
 import { FormsModule } from '@angular/forms';
+import { ResultadoComponent } from './resultado/resultado.component';
 
 @NgModule({
   declarations: [
-    AppComponent, PersonasComponent, PersonaComponent
+    AppComponent, PersonasComponent, PersonaComponent, ResultadoComponent
   ],
   imports: [
     BrowserModule,
